72f93af995792cb52bc56bb1955bf15f66b97cc2
TorokJani <90218427+TorokJani@users.noreply.github.com>
2021-11-19 13:45:14 +0100

    new branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-
-
     }
+
+    public int countVocabulary(List<String> words) {
+        Set<String> wordSet = new HashSet<>(words);
+
+       /* Set<String> wordSet = new HashSet<>();
+        for ( String w:words){
+            wordSet.add(w);
+        }*/
+    return wordSet.size();
+    }
+
 }
 
